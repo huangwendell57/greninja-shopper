355146af42a6b8695daca3bf74bdfbe4812b8666
huangwendell57 <huangwendell57@gmail.com>
2026-05-06 01:14:16 +1200

    Update GreninjaShopperEntity.java

--- src/main/java/zhua841/greninjashopper/entity/GreninjaShopperEntity.java
+++ src/main/java/zhua841/greninjashopper/entity/GreninjaShopperEntity.java
@@ -3,17 +3,17 @@ package zhua841.greninjashopper.entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.LookAroundGoal;
 import net.minecraft.entity.ai.goal.LookAtEntityGoal;
+import net.minecraft.entity.ai.goal.WanderAroundFarGoal;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.mob.PathAwareEntity;
-import net.minecraft.entity.passive.PassiveEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoEntity;
 import software.bernie.geckolib.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.animation.AnimatableManager;
+import software.bernie.geckolib.animation.AnimationController;
+import software.bernie.geckolib.animation.PlayState;
 import software.bernie.geckolib.util.GeckoLibUtil;
 
 public class GreninjaShopperEntity extends PathAwareEntity implements GeoEntity {
@@ -21,36 +21,29 @@ public class GreninjaShopperEntity extends PathAwareEntity implements GeoEntity 
 
     public GreninjaShopperEntity(EntityType<? extends PathAwareEntity> entityType, World world) {
         super(entityType, world);
-        this.setAiDisabled(false);
     }
 
-    public static DefaultAttributeContainer.Builder createGreninjaAttributes() {
+    public static DefaultAttributeContainer.Builder createGreninjaShopperAttributes() {
         return PathAwareEntity.createMobAttributes()
                 .add(EntityAttributes.GENERIC_MAX_HEALTH, 20.0D)
-                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.0D)
-                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 8.0D);
+                .add(EntityAttributes.GENERIC_MOVEMENT_SPEED, 0.25D)
+                .add(EntityAttributes.GENERIC_FOLLOW_RANGE, 16.0D);
     }
 
     @Override
     protected void initGoals() {
-        this.goalSelector.add(1, new LookAtEntityGoal(this, PlayerEntity.class, 6.0F));
+        this.goalSelector.add(1, new LookAtEntityGoal(this, PlayerEntity.class, 8.0F));
         this.goalSelector.add(2, new LookAroundGoal(this));
+        this.goalSelector.add(3, new WanderAroundFarGoal(this, 0.8D));
     }
 
     @Override
     public void registerControllers(AnimatableManager.ControllerRegistrar controllers) {
-        // Static display entity: no animation controller needed.
-        // The model can still be rendered by Easy NPC's Doppler custom model system.
+        controllers.add(new AnimationController<>(this, "controller", 0, state -> PlayState.CONTINUE));
     }
 
     @Override
     public AnimatableInstanceCache getAnimatableInstanceCache() {
         return this.cache;
-    }
-
-    @Nullable
-    @Override
-    public PassiveEntity createChild(ServerWorld world, PassiveEntity entity) {
-        return null;
     }
 }
